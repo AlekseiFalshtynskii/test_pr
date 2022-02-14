cbe0af16741a1d1ee44a7ca0d8177aacfaf2bde3
Aleksei Falshtynskii <aleksei.falshtynskii@gmail.com>
2022-02-14 10:14:12 +0300

    branch3

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -1,10 +1,25 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Test {
 
-    private Boolean clntGrpDetail;
-
     public static void main(String[] args) {
-        System.out.println(new Test().clntGrpDetail);
+        System.out.println(squareDigits("2"));
+        System.out.println(squareDigits("a2b"));
+        System.out.println(squareDigits("a22b"));
+        System.out.println(squareDigits("a9b2"));
+    }
+
+    private static String squareDigits(String s) {
+        return Arrays.stream(s.split("")).map(c -> {
+            try {
+                int n = Integer.parseInt(c);
+                return String.valueOf(n * n);
+            } catch (NumberFormatException e) {
+                return c;
+            }
+        }).collect(Collectors.joining());
     }
 }
